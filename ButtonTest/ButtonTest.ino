acad3dccaa4160d6e8962486005072e377de84a7
Charlie Forkish <charlie@playpiper.com>
2019-05-17 11:34:09 -0700

    renamed JoystickTest, added ability to select different buttons in ButtonTest

--- ButtonTest/ButtonTest.ino
+++ ButtonTest/ButtonTest.ino
@@ -2,34 +2,52 @@
   ButtonTest
 
   Turns on the Aruduino on-board Light Emitting Diode (LED) connected to 
-  digital pin 13, when button No.1 (UP) connected to pin 6 is pressed.
+  digital pin 13, when UP button (No.1) connected to pin 6 is pressed.
 
 */
 
-// Constants won't change. They're used here to set pin numbers:
-const int buttonPin = 6;     // The number of the UP button pin
-const int ledPin =  13;      // The number of the LED pin
+/////////////////////////////////////////////////////////////////////////
+// Pin mappings to Arduino Micro according to the assembly instructions
+enum ArduinoPinMap
+{
+  // Click & Button Pins
+  LeftButtonPin = 8,
+  RightButtonPin = 9,
+  UpButtonPin = 6,
+  DownButtonPin = 7,
+  LEDPin = 13,
+};
+/////////////////////////////////////////////////////////////////////////
 
-// Variables will change:
-int buttonState = 0;         // A variable for reading the button state
+// This constant sets which button will activate the LED.
+// Try changing it to one of the other pins to test the other buttons!
+const int selectedButton = UpButtonPin;
 
-void setup() {
+// A variable for reading the button state
+int buttonState = 0;
+
+void setup()
+{
   // Initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
+  pinMode(LEDPin, OUTPUT);
   // Initialize the button pin as an input:
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(selectedButton, INPUT_PULLUP);
 }
 
-void loop() {
+void loop()
+{
   // Read the state of the button pin:
-  buttonState = digitalRead(buttonPin);
+  buttonState = digitalRead(selectedButton);
 
   // Check if the button is pressed. If it is, the buttonState is LOW:
-  if (buttonState == LOW) {
+  if (buttonState == LOW)
+  {
     // Turn the LED on by making the voltage HIGH:
-    digitalWrite(ledPin, HIGH);
-  } else {
+    digitalWrite(LEDPin, HIGH);
+  }
+  else
+  {
     // Turn the LED off by making the voltage LOW:
-    digitalWrite(ledPin, LOW);
+    digitalWrite(LEDPin, LOW);
   }
 }
